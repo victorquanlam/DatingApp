20f558ed89d9e35953b935ce38c7cc69e06c7c61
peterjones <quanphuoc89@gmail.com>
2021-08-30 03:30:21 +1000

    yeah nah

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  @Input() usersFromHomeComponent: any;
+  @Output() cancelRegister = new EventEmitter();
+
+  model: any = {};
+
+  constructor(private accountService: AccountService, private toastr: ToastrService) { }
+
+  ngOnInit(): void {
+  }
+
+  register() {
+    this.accountService.register(this.model).subscribe(res => {
+      this.cancel()
+    }, err => {
+      this.toastr.error(err.error || err.errors[1])
+    })
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false)
+  }
+
+}
